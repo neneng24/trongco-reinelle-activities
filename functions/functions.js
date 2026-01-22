2deafd75d72becda8bc13bb8c78fa3bcbf12131f
Reinelle Trongco <reinelletrongco@gmail.com>
2026-01-22 11:44:19 +0800

    calculator

---
+++ functions/functions.js
@@ -0,0 +1,13 @@
+function normal() {
+    const sum = getSum(10, 20);
+    alert(sum)
+}
+
+function withParameter(fname, lname) {
+    alert("Hello " + fname + " " + lname);
+}
+
+function getSum(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
